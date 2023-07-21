7d7b3adb291d64c6953cf33ebae11bca1017b2f8
Dhawan Solanki <dhawansolanki2103@gmail.com>
2023-07-21 10:44:36 +0530

    Contact Fixed

--- src/Contact.js
+++ src/Contact.js
@@ -5,27 +5,29 @@ function Contact() {
   return (
     <div class="container d-flex justify-content-center align-items-center">
 		<img src="Images/Contact.png" alt="Bloom The Grapheteria" class="Contactus" />  
-	<form className='contactform'>
+	<form className='contactform' action='https://formspree.io/f/xknlgjne'>
 			<div class="form-group position-relative">
-				<label for="formName" class="d-block">
-					<i class="icon" data-feather="user"></i>
-				</label>
+			<h1 className='contactlabeltext'>Name</h1>
 				<input type="text" id="formName" class="form-control form-control-lg thick" placeholder="Enter your Name"/>
 			</div>
 			<br/>
 			<div class="form-group position-relative">
-				<label for="formEmail" class="d-block">
-					<i class="icon" data-feather="mail"></i>
-				</label>
+			<h1 className='contactlabeltext'>Email</h1>
 				<input type="email" id="formEmail" class="form-control form-control-lg thick" placeholder="Enter your Email"/>
 			</div>
 		<br/>
+			<div class="form-group position-relative">
+			<h1 className='contactlabeltext'>Service</h1>
+				<input type="text" id="formService" class="form-control form-control-lg thick" placeholder="Enter Service"/>
+			</div>
+		<br/>
 			<div class="form-group message">
-				<textarea id="formMessage" class="form-control form-control-lg" rows="15" placeholder=""></textarea>
+			<h1 className='contactlabeltext'>Message</h1>
+				<textarea id="formMessage" class="form-control form-control-lg" rows="10" placeholder=""></textarea>
 			</div>
 			<br/>
 			<div class="text-center">
-				<button type="submit" class="btn btn-primary" tabIndex="-1">Submit</button>
+				<button type="submit" class="btn btn-primary contactsubmitbtn" tabIndex="-1">Submit</button>
 			</div>
 	</form>
 	
