6c63d5f1566af7af308c82d976bf886b4fa9c4c0
Dhawan Solanki <dhawansolanki2103@gmail.com>
2023-07-21 10:56:44 +0530

    Contact Form Solved

--- src/Contact.js
+++ src/Contact.js
@@ -4,34 +4,35 @@ import "./styles/contact.scss"
 function Contact() {
   return (
     <div class="container d-flex justify-content-center align-items-center">
-		<img src="Images/Contact.png" alt="Bloom The Grapheteria" class="Contactus" />  
-	<form className='contactform' action='https://formspree.io/f/xknlgjne'>
-			<div class="form-group position-relative">
-			<h1 className='contactlabeltext'>Name</h1>
-				<input type="text" id="formName" class="form-control form-control-lg thick" placeholder="Enter your Name"/>
-			</div>
-			<br/>
-			<div class="form-group position-relative">
-			<h1 className='contactlabeltext'>Email</h1>
-				<input type="email" id="formEmail" class="form-control form-control-lg thick" placeholder="Enter your Email"/>
-			</div>
+  <img src="Images/Contact.png" alt="Bloom The Grapheteria" class="Contactus" />
+  <form class="contactform" action="https://formspree.io/f/xknlgjne">
+    <div class="form-row">
+      <div class="form-group col">
+        <h1 class="contactlabeltext">Name</h1>
+		<br/><br/>
+        <h1 class="contactlabeltext">Email</h1>
+		<br/><br/>
+        <h1 class="contactlabeltext">Service</h1>
 		<br/>
-			<div class="form-group position-relative">
-			<h1 className='contactlabeltext'>Service</h1>
-				<input type="text" id="formService" class="form-control form-control-lg thick" placeholder="Enter Service"/>
-			</div>
+        <h1 class="contactlabeltext">Message</h1>
+      </div>
+      <div class="form-group col">
+        <input type="text" id="formName" class="form-control form-control-lg thick" placeholder="Enter your Name" />
 		<br/>
-			<div class="form-group message">
-			<h1 className='contactlabeltext'>Message</h1>
-				<textarea id="formMessage" class="form-control form-control-lg" rows="10" placeholder=""></textarea>
-			</div>
-			<br/>
-			<div class="text-center">
-				<button type="submit" class="btn btn-primary contactsubmitbtn" tabIndex="-1">Submit</button>
-			</div>
-	</form>
-	
+        <input type="email" id="formEmail" class="form-control form-control-lg thick" placeholder="Enter your Email" />
+		<br/>
+        <input type="text" id="formService" class="form-control form-control-lg thick" placeholder="Enter Service" />
+		<br/>
+        <textarea id="formMessage" class="form-control form-control-lg" rows="10" placeholder=""></textarea>
+		<br/>
+      </div>
+    </div>
+    <div class="text-right">
+      <button type="submit" class="btn btn-primary contactsubmitbtn" tabIndex="-1">Submit</button>
+    </div>
+  </form>
 </div>
+
   )
 }
 
